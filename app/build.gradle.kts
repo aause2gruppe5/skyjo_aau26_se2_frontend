4b99d01eed18d6cb53d62d354fc342a78d1047e6
Jakob Keischnigg <jakobkeischnigg@gmail.com>
2026-04-08 21:53:02 +0200

    Added missing lib

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.compose.runtime:runtime-android:1.10.6")
 
     // Jetpack Compose
     val composeBom = platform("androidx.compose:compose-bom:$composeBomVersion")
