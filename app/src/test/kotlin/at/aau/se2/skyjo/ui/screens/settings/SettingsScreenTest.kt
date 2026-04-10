cfa4bde3a6fc74f817d22ccc3a4a6b9105dbcb46
raphaelbleier <raphaelbl@edu.aau.at>
2026-04-10 12:28:46 +0200

    test(settings): expand SettingsScreenTest with content assertions

--- app/src/test/kotlin/at/aau/se2/skyjo/ui/screens/settings/SettingsScreenTest.kt
+++ app/src/test/kotlin/at/aau/se2/skyjo/ui/screens/settings/SettingsScreenTest.kt
@@ -29,25 +29,103 @@ class SettingsScreenTest {
     }
 
     @Test
-    fun settingsScreen_compose_tree_is_complete() {
+    fun settingsScreen_shows_settings_title() {
         composeTestRule.setContent {
             SkyjoTheme {
                 SettingsScreen(onNavigate = {})
             }
         }
-        composeTestRule.waitForIdle()
-        composeTestRule.onRoot().assertIsDisplayed()
+        composeTestRule.onNodeWithText("Settings").assertIsDisplayed()
     }
 
     @Test
-    fun settingsScreen_renders_with_default_state() {
+    fun settingsScreen_shows_player_name() {
         composeTestRule.setContent {
             SkyjoTheme {
                 SettingsScreen(onNavigate = {})
             }
         }
-        composeTestRule.waitForIdle()
-        composeTestRule.onRoot().assertIsDisplayed()
+        composeTestRule.onNodeWithText("AcePlayer").assertIsDisplayed()
+    }
+
+    @Test
+    fun settingsScreen_shows_player_level() {
+        composeTestRule.setContent {
+            SkyjoTheme {
+                SettingsScreen(onNavigate = {})
+            }
+        }
+        composeTestRule.onNodeWithText("Level 42").assertIsDisplayed()
+    }
+
+    @Test
+    fun settingsScreen_shows_game_preferences_section() {
+        composeTestRule.setContent {
+            SkyjoTheme {
+                SettingsScreen(onNavigate = {})
+            }
+        }
+        composeTestRule.onNodeWithText("Game Preferences").assertIsDisplayed()
+    }
+
+    @Test
+    fun settingsScreen_shows_sound_fx_toggle() {
+        composeTestRule.setContent {
+            SkyjoTheme {
+                SettingsScreen(onNavigate = {})
+            }
+        }
+        composeTestRule.onNodeWithText("Sound FX").assertIsDisplayed()
+    }
+
+    @Test
+    fun settingsScreen_shows_music_toggle() {
+        composeTestRule.setContent {
+            SkyjoTheme {
+                SettingsScreen(onNavigate = {})
+            }
+        }
+        composeTestRule.onNodeWithText("Music").assertIsDisplayed()
+    }
+
+    @Test
+    fun settingsScreen_shows_haptic_feedback_toggle() {
+        composeTestRule.setContent {
+            SkyjoTheme {
+                SettingsScreen(onNavigate = {})
+            }
+        }
+        composeTestRule.onNodeWithText("Haptic Feedback").assertIsDisplayed()
+    }
+
+    @Test
+    fun settingsScreen_shows_account_section() {
+        composeTestRule.setContent {
+            SkyjoTheme {
+                SettingsScreen(onNavigate = {})
+            }
+        }
+        composeTestRule.onNodeWithText("Account").assertIsDisplayed()
+    }
+
+    @Test
+    fun settingsScreen_shows_link_account_option() {
+        composeTestRule.setContent {
+            SkyjoTheme {
+                SettingsScreen(onNavigate = {})
+            }
+        }
+        composeTestRule.onNodeWithText("Link Account").assertIsDisplayed()
+    }
+
+    @Test
+    fun settingsScreen_shows_app_version() {
+        composeTestRule.setContent {
+            SkyjoTheme {
+                SettingsScreen(onNavigate = {})
+            }
+        }
+        composeTestRule.onNodeWithText("Skyjo Action v1.0.0").assertIsDisplayed()
     }
 
     @Test
